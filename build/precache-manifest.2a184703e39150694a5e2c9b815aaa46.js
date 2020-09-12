5171111897445e413b57c355f96809ec13f22b09
justinas.geryba@gmail.com <justinas.geryba@gmail.com>
2020-09-12 20:38:40 +0300

    .

---
+++ build/precache-manifest.2a184703e39150694a5e2c9b815aaa46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed129ac59206760ba0264761c74c160",
+    "url": "/arabiccourses/index.html"
+  },
+  {
+    "revision": "f0a00adce3077bbc30da",
+    "url": "/arabiccourses/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d303ab3cd7fd8aadcab1",
+    "url": "/arabiccourses/static/js/2.fe897e42.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/arabiccourses/static/js/2.fe897e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a00adce3077bbc30da",
+    "url": "/arabiccourses/static/js/main.b753e661.chunk.js"
+  },
+  {
+    "revision": "a77912133a275b04b6d2",
+    "url": "/arabiccourses/static/js/runtime-main.406cf174.js"
+  },
+  {
+    "revision": "9c9f2c30092c281bd86a9728ce29e690",
+    "url": "/arabiccourses/static/media/arabicLogo.9c9f2c30.png"
+  },
+  {
+    "revision": "baf2b66ae08a16d403c1f74dbb001159",
+    "url": "/arabiccourses/static/media/avatar.baf2b66a.jpg"
+  },
+  {
+    "revision": "4ac9d773cd1c4fd5558f426006169fe0",
+    "url": "/arabiccourses/static/media/desert.4ac9d773.jpg"
+  },
+  {
+    "revision": "6c7dfa608279396d77fa566a438ac8e5",
+    "url": "/arabiccourses/static/media/skype.6c7dfa60.png"
+  }
+]);
