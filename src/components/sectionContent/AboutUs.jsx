dc8f7bf3a7a19b6eb79e7c47250522e6c9c24305
justinas.geryba@gmail.com <justinas.geryba@gmail.com>
2020-09-18 21:49:16 +0300

    final adjustment of 'About us' image size for tablets/phones

--- src/components/sectionContent/AboutUs.jsx
+++ src/components/sectionContent/AboutUs.jsx
@@ -4,19 +4,20 @@ import {useMediaPredicate} from "react-media-hook";
 
 
 const AboutUs = (props) => {
-    const screensizeSM = useMediaPredicate("(min-width: 400px)");
-    const screensizeMD = useMediaPredicate("(min-width: 500px)");
+    const screensize = useMediaPredicate("(min-width: 400px)");
+    const screensizeForImage = useMediaPredicate("(min-width: 600px)");
+
     const textStyle = {
-        lineHeight: screensizeSM ? '2.5rem' : '2rem',
+        lineHeight: screensize ? '2.5rem' : '2rem',
         fontWeight: '600',
-        fontSize: screensizeSM ? '1.2rem' : '1.1rem'
+        fontSize: screensize ? '1.2rem' : '1.1rem'
     };
     const imageStyle = {
-        shapeOutside: screensizeMD ? 'circle(50%)' : 'none',
+        shapeOutside: 'circle(50%)',
         borderRadius: '50%',
         overflow: 'hidden',
-        float: screensizeMD ? 'right' : 'inherit',
-        width: screensizeMD ? '50vh' : '20vh'
+        float: 'right',
+        width: screensizeForImage ? '50vh' : '50vw'
     };
 
     return <div style={{display:'inline-block'}}>
