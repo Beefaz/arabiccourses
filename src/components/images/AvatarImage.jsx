182c46e339aefa197110d2a5c31bd78760301428
justinas.geryba@gmail.com <justinas.geryba@gmail.com>
2020-12-09 15:57:08 +0200

    added container to avatar, because text was touching it on some screens. changed icon route

--- src/components/images/AvatarImage.jsx
+++ src/components/images/AvatarImage.jsx
@@ -2,21 +2,27 @@ import React from "react";
 
 
 const AvatarImage = (props) => {
-    const imageStyle = {
+    const containerStyle = {
         boxShadow: '1px 1px 5px 1px, -1px -1px 5px 1px',
         shapeOutside: 'circle(50%)',
         borderRadius: '50%',
-        overflow: 'hidden',
         float: props.float,
         height: '50vmin',
         width: '50vmin',
-        borderColor: 'rgba(0,0,0,0.3)',
-        borderWidth: '3px',
-        borderStyle: 'solid'
+        margin: '10px',
+        overflow: 'hidden'
     };
-
-    return <img style={imageStyle}
-             src={props.src}
-             alt=""/>
+    const imageStyle = {
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        padding: '3px',
+        borderRadius: '50%',
+        width: '100%',
+      objectFit: 'cover',
+    };
+    return <div style={containerStyle}>
+        <img style={imageStyle}
+                src={props.src}
+                alt=""/>
+    </div>
 };
 export default AvatarImage;
