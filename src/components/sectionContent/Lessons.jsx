642690792ba973e070fbbd12646de57e2c6fb43b
Beefaz <justinas.geryba@gmail.com>
2021-01-25 16:52:40 +0200

    added styling grid in video section

--- src/components/sectionContent/Lessons.jsx
+++ src/components/sectionContent/Lessons.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import loadable$2 from "@loadable/component";
-const IframeBlock =  loadable$2(() => import("../iframeBlock/IframeBlock"));
+import {Row, Col} from "react-bootstrap";
+
+const IframeBlock = loadable$2(() => import("../iframeBlock/IframeBlock"));
 
 const Lessons = (props) => {
+    const colStyle = {
+        padding: '1rem',
+    };
     return <div>
         {props.label}
-        <IframeBlock title='Promo' url={'https://www.youtube.com/embed/4ybn3kKuu3I'}/>
-        <IframeBlock title='Lesson' url={'https://www.youtube.com/embed/4-h4QW0J_Fk'}/>
+        <Row>
+            <Col xs={12} md={6} style={colStyle} >
+                <IframeBlock title='Promo' url={'https://www.youtube.com/embed/4ybn3kKuu3I'}/>
+            </Col>
+            <Col xs={12} md={6} style={colStyle} >
+                <IframeBlock title='Lesson' url={'https://www.youtube.com/embed/QqkuP_i9IsU'}/>
+            </Col>
+        </Row>
     </div>
 };
 export default Lessons;
